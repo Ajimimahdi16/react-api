7c3ce22b7f33886b604e6668da8e4aa5a664f8c0
Ajimi Mahdi <ajimimahdi1@gmail.com>
2026-03-04 20:27:28 +0100

    Implementata la struttura principale del componente App includendo un titolo h1 e una sezione dedicata al rendering degli attori. Utilizzando il metodo .map(), i dati recuperati dall'API (collegati tramite actor.id) vengono trasformati in card dinamiche contenenti l'immagine dell'attore e una sezione informativa. Oltre ai requisiti di base (nome in h2, anno di nascita, nazionalità, biografia e riconoscimenti), è stata aggiunta la visualizzazione dei film per cui l'attore è conosciuto, formattando gli array dei dati tramite il metodo .join() per la leggibilita

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from "axios";
+import "./app.css";
+
 
 function App() {
   const [actors, setActors] = useState([]); 
@@ -16,10 +18,27 @@ function App() {
   useEffect(fetchActors, []);
 
   return (
-    <>
-      <h1>Lista Attori</h1>
-    
-    </>
+    <main>
+      <h1 className='titolo'>Lista di Attori</h1>
+      <section className='actor'>
+        {actors.map(actor => (
+          <article  key={actor.id}className="actor-card" > 
+            <div className='img-container'>
+              <img src={actor.image} alt= "" />
+            </div>
+            
+            <div className='info'>
+              <h2>{actor.name}</h2>
+              <p><strong>Anno di nascita:</strong> {actor.birth_year}</p>
+              <p><strong>Nazionalità:</strong> {actor.nationality}</p>
+              <p><strong>Biografia:</strong> {actor.biography}</p>
+              <p><strong>Riconoscimenti:</strong> {actor.awards.join(" - ")}</p>
+              <p><strong>Conosciuto per:</strong> {actor.known_for.join(" - ")}</p>
+            </div>
+          </article>
+        ))}
+      </section>
+    </main>
   );
 }
 
